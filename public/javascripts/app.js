089385d07c8b7b0d5787758f95e90d34fd1927ba
Jacqueline <jacqueline@linuxlab>
2013-12-28 13:37:15 +0100

    new

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,9 @@
 var reMemberApp = angular.module('reMemberApp',[]);
  
-reMemberApp.controller('WelcomeController', function ($scope) {
+reMemberApp.controller('WelcomeController', function ($scope,$http) {
      $scope.title = "The cat just made a strange noise";
      $scope.story = "We better check her out and see if she's ok."
+
+
+     $http.get('http://localhost:3000/members').success(function(data) { $scope.members = data; });
 });
